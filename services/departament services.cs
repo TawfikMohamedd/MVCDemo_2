995900de0b0654778f11cbd51e5cb752b526d39e
Tawfik <Tawfik@DESKTOP-7CNV70R>
2024-11-05 21:07:42 +0200

    Session 4

---
+++ services/departament services.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Company.Service.Services
+{
+public class DepartmentService : IDeparmentService
+    {
+        private readonly IDepartmentRepository departmentRepository;
+        
+public DepartmentService(IDepartment Repository departmentRepository)
+        {
+            departmentRepository;
+            _departmentRepository
+public void Add(Department employee)
+throw new NotImplementedException();
+            1 reference
+public void Delete(Department employee)
+            {
+            }
+            throw new NotImplementedException();
+            
+public IEnumerable<Department> GetAll()
+            {
+                throw new NotImplementedException();
